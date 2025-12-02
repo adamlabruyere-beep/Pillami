ffd485da91d2319a7f600bf0e66801bbdffe9ca9
Adam Labruyere <adam.labruyere@gmail.com>
2025-12-02 15:05:58 +0100

    del calendrier col created & upd

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_02_091924) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
