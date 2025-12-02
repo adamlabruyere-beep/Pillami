d980758114e49cb42229f1c7757de7bf0ace5263
Adam Labruyere <adam.labruyere@gmail.com>
2025-12-02 17:15:00 +0100

    first link + home button

--- db/migrate/20251202141117_create_medicaments.rb
+++ db/migrate/20251202141117_create_medicaments.rb
@@ -1,6 +1,6 @@
 class CreateMedicaments < ActiveRecord::Migration[7.1]
   def change
-    create_table :medicaments do |t|
+    create_table :medicaments, if_not_exists: true do |t|
       t.string :nom
       t.string :format
       t.string :prise
--- db/schema.rb
+++ db/schema.rb
@@ -14,15 +14,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_02_150342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "medicaments", force: :cascade do |t|
-    t.string "nom"
-    t.string "format"
-    t.boolean "ordonnance"
+  create_table "calendriers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "prise", default: [], array: true
+  end
 
-  create_table "calendriers", force: :cascade do |t|
+  create_table "medicaments", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
