c2236f0f8f6061ffd536477f63dcacb91bd777fc
Imen Bounab <imen.bounab@gmail.com>
2025-12-04 12:33:21 +0100

    routes nestées rappel

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,4 +1,6 @@
 class Reminder < ApplicationRecord
   belongs_to :user
   belongs_to :medicament
+  serialize :days_of_week, type: Array, coder: YAML
+  validates :time, presence: { message: "L'heure du rappel est obligatoire" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :calendriers, only: [:index]
-  resources :reminders
+  resources :users do
+    resources :reminders
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
