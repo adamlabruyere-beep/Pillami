a866896ba70967c4303ca1ba8ef444b24ab5bd11
maxence maho <maxencemaho@gmail.com>
2025-12-05 10:08:22 +0100

    button a droite et logo pillami rootpath

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["nav", "overlay", "openIcon", "closeIcon"]
+  static targets = ["nav", "overlay", "openIcon", "closeIcon", "logo", "logoPlaceholder"]
 
   toggle() {
     if (this.navTarget.classList.contains("-translate-x-full")) {
@@ -18,6 +18,19 @@ export default class extends Controller {
     this.openIconTarget.classList.add("hidden")
     this.closeIconTarget.classList.remove("hidden")
     document.body.classList.add("overflow-hidden")
+
+    // Animation du logo: disparaît du header
+    if (this.hasLogoTarget) {
+      this.logoTarget.classList.add("opacity-0", "-translate-y-2")
+    }
+
+    // Animation du logo: apparaît dans la navbar
+    if (this.hasLogoPlaceholderTarget) {
+      setTimeout(() => {
+        this.logoPlaceholderTarget.classList.remove("opacity-0", "-translate-y-4")
+        this.logoPlaceholderTarget.classList.add("opacity-100", "translate-y-0")
+      }, 150)
+    }
   }
 
   close() {
@@ -27,5 +40,16 @@ export default class extends Controller {
     this.openIconTarget.classList.remove("hidden")
     this.closeIconTarget.classList.add("hidden")
     document.body.classList.remove("overflow-hidden")
+
+    // Animation du logo: réapparaît dans le header
+    if (this.hasLogoTarget) {
+      this.logoTarget.classList.remove("opacity-0", "-translate-y-2")
+    }
+
+    // Animation du logo: disparaît de la navbar
+    if (this.hasLogoPlaceholderTarget) {
+      this.logoPlaceholderTarget.classList.add("opacity-0", "-translate-y-4")
+      this.logoPlaceholderTarget.classList.remove("opacity-100", "translate-y-0")
+    }
   }
 }
