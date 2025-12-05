352cc48f7e1b311bac4d12387d00448d6e62e1df
maxence maho <maxencemaho@gmail.com>
2025-12-05 10:56:48 +0100

    seed 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -76,7 +76,7 @@ MEDICAMENTS_COURANTS = [
 # avec le nom du médicament affiché lors de l'arrêt
 # Mettre nil pour commencer depuis le début
 # ============================================================
-REPRENDRE_DEPUIS = nil # ex: "amoxicilline"
+ REPRENDRE_DEPUIS = "diclofenac" 
 
 MAX_RETRIES_429 = 5
 
