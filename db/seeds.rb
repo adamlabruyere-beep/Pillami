0e4a1be88f6d23d0d61eaac6e7bab4451b284096
maxence maho <maxencemaho@gmail.com>
2025-12-08 15:29:18 +0100

    seed collyre

--- db/seeds.rb
+++ db/seeds.rb
@@ -76,7 +76,7 @@ MEDICAMENTS_COURANTS = [
 # avec le nom du médicament affiché lors de l'arrêt
 # Mettre nil pour commencer depuis le début
 # ============================================================
-REPRENDRE_DEPUIS = "triptan"
+REPRENDRE_DEPUIS = "collyre"
 
 MAX_RETRIES_429 = 5
 
