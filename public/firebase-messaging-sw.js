3d7b55a67b3c497668ac52c36d0c175c8a4bb938
maxence maho <maxencemaho@gmail.com>
2025-12-09 13:05:42 +0100

    Fix Firebase SW version mismatch

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,18 +1,22 @@
-importScripts("https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js")
-importScripts("https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-compat.js")
+importScripts("https://www.gstatic.com/firebasejs/10.13.0/firebase-app-compat.js")
+importScripts("https://www.gstatic.com/firebasejs/10.13.0/firebase-messaging-compat.js")
 
 firebase.initializeApp({
   apiKey: "AIzaSyAwpB3lK5tNUgkqvnQWtOL91tP9IfRWDDE",
   authDomain: "pillami-e98ac.firebaseapp.com",
   projectId: "pillami-e98ac",
+  storageBucket: "pillami-e98ac.firebasestorage.app",
   messagingSenderId: "583918755836",
-  appId: "1:583918755836:web:30cf6f69ebfe7f715b1c79"
+  appId: "1:583918755836:web:30cf6f69ebfe7f715b1c79",
+  measurementId: "G-6FKSN2SZ8F"
 })
 
 const messaging = firebase.messaging()
 
 messaging.onBackgroundMessage(function(payload) {
+  console.log("📩 Message reçu en background:", payload)
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
+    icon: "/favicon-32x32.png"
   })
 })
