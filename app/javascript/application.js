9ea7e2a6e107df144f55ec55d735dd1835412076
maxence maho <maxencemaho@gmail.com>
2025-12-09 12:16:50 +0100

    firebase

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./firebase"
+import "firebase"
 
 document.addEventListener("turbo:load", () => {
   if (window.currentUserLoggedIn) { // tu peux exposer ça via un helper
